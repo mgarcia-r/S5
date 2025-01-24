ca5992403486a993d5516af8dff3feb928755766
Marinagr <marina.antela@gmail.com>
2025-01-24 13:22:46 +0100

    dice game

---
+++ .venv/while_example.py
@@ -0,0 +1,21 @@
+#You have 3 lives.I roll the dice. If I roll 6 you win.
+#If it's not a 6, you lose I live
+
+from random import randint
+lives= 3
+while lives:
+    roll= randint(1,6)
+    if roll==6:
+        print("You rolled a 6! You win!")
+        break #this exists the while even if lives still>0
+    # there is no other way to get here unless I DID NOT roll a 6
+    print(f"You rolled a {roll}! You lose a life")
+    lives-=1
+    print(f"lives left: {lives}")
+else: #else from while!!
+    print("You lost!")
+
+
+
+
+
